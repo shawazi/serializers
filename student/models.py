5ae559860057b570e3db6e0abfd2f694f2c7266c
“shawazi” <shawazisonfire@gmail.com>
2023-05-10 19:43:20 -0400

    initialized

---
+++ student/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Path(models.Model):
+    name = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.name
+    
+class Student(models.Model):
+    path = models.ForeignKey(Path, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    number = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
